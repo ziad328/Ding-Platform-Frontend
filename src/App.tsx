a4cf393f2b9911452b27546140af16e1b60d3e9d
ziadseleem <ziad32seleem@gmail.com>
2025-10-17 19:02:31 +0300

    feat: project initialization and styling setup
    - Scaffold React project with Vite
    - Create folder structure and install dependencies
    - Configure Tailwind CSS theme and colors

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center pt-40">
+
+        <h1 className="text-3xl text-primary-800 font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
