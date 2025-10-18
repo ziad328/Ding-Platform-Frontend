f2024c5f85550f8db142a616544bf5c4c527198e
ziadseleem <ziad32seleem@gmail.com>
2025-10-18 15:31:57 +0300

    feat: set up routing and add empty pages for home, profile, and not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/home/HomePage'
+import MainLayout from './layout/MainLayout';
+import NotFoundPage from './pages/notfound/NotFoundPage';
+import ProfilePage from './pages/profile/ProfilePage'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />} >
+      <Route index element={<HomePage />} />
+      <Route path='*' element={<NotFoundPage />} />
+      <Route path='/about' element={<ProfilePage />} />
+    </Route>
+  )
+);
+
 
 function App() {
 
   return (
     <>
-      <div className="text-center pt-40">
-
-        <h1 className="text-3xl text-primary-800 font-bold underline">
-          Hello world!
-        </h1>
-      </div>
+      <RouterProvider router={router} />
     </>
   )
 }
